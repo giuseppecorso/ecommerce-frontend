66b655309936ed5e17f7d36df93508479a94adc2
Giuseppe Corso <giuseppecorso91@gmail.com>
2026-09-25 17:51:11 +0200

    Add Angular Material with toolbar and product cards

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,9 +1,10 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet, RouterLink } from '@angular/router';
+import { MatToolbarModule } from '@angular/material/toolbar';
 
 
 @Component({
-  imports: [RouterOutlet, RouterLink],
+  imports: [RouterOutlet, RouterLink, MatToolbarModule],
   selector: 'app-root',
   styleUrl: './app.css',
   templateUrl: './app.html',
--- src/app/product-list/product-list.ts
+++ src/app/product-list/product-list.ts
@@ -4,10 +4,12 @@ import { Product } from '../product';
 import { RouterLink } from '@angular/router';
 import { environment } from '../../environments/environment';
 import { CurrencyPipe } from '@angular/common';
+import { MatCardModule } from '@angular/material/card';
+import { MatButtonModule } from '@angular/material/button';
 
 @Component({
   selector: 'app-product-list',
-  imports: [RouterLink, CurrencyPipe],
+  imports: [RouterLink, CurrencyPipe, MatCardModule, MatButtonModule],
   templateUrl: './product-list.html',
   styleUrl: './product-list.css',
 })
